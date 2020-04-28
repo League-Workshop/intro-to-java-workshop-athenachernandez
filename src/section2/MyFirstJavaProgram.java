f0d16010d5369a148c786376a637945c85d867e1
root <root@61d085847be1>
2020-04-28 21:46:57 +0000

    Finished everything.

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+		Robot rob = new Robot();
 		
+		rob.penDown();
+		// rob.penUp();
+		rob.setSpeed(100);
+		
+		for (int i = 0; i < 4; i++) {
+			rob.move(200);
+			rob.turn(90);
+		}
 		
 	}
 }
